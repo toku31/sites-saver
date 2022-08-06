3c37d76d47c79aaa1b4c2632263f8f1e2b6b77cf
user <palmtree8880@gmail.com>
2022-08-06 17:19:57 +0900

    second deployment

--- sitesSaver/settings.py
+++ sitesSaver/settings.py
@@ -175,8 +175,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'maple9877@gmail.com'
-EMAIL_HOST_PASSWORD = '!apple135'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
 
 
 # Static files (CSS, JavaScript, Images)
